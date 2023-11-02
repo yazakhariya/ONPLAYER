ac19902f87a9d2efdeacac39154cbe51486d5516
yazakhariya <17070501.iana@gmail.com>
2023-11-02 23:48:23 +0300

    feature: notfound page done

--- src/pages/notfound/NotFound.tsx
+++ src/pages/notfound/NotFound.tsx
@@ -1,3 +1,10 @@
+import error from 'src/assets/error.png'
+import s from './NotFound.module.css'
+
 export default function NotFound() {
-  return <div>NotFound</div>
+  return (
+    <div className={s.wrapper}>
+      <img className={s.error} src={error} />
+    </div>
+  )
 }
